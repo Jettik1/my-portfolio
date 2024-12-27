6d8a6bbe974726b591079771157f66d89690d730
Jettik1 <contrakt120@gmail.com>
2024-12-27 13:25:02 +0300

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './index.css'
 import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
@@ -96,30 +95,32 @@ const App = () => {
     <div className="min-h-screen bg-background text-primary">
       <header className="p-4 bg-header flex justify-between items-center">
         <h1 className="text-xl font-bold">My Portfolio</h1>
-        <button
-          onClick={toggleTheme}
-          className="px-4 py-2 bg-button text-buttonText rounded"
-        >
-          {theme === 'light' ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-yellow-500"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2a7 7 0 00-7 7c0 2.263 1.211 4.443 3.004 5.784A1.984 1.984 0 008 16v1a1 1 0 001 1h6a1 1 0 001-1v-1a1.984 1.984 0 00-.004-.216C17.789 13.443 19 11.263 19 9a7 7 0 00-7-7zm-2 17v2h4v-2h-4zm8.707-13.293l-1.414 1.414A8.964 8.964 0 0119 9a9 9 0 01-.293 2.707l1.414 1.414A10.935 10.935 0 0021 9a10.935 10.935 0 00-1.293-3.293zM5.707 4.293A10.935 10.935 0 004 9c0 1.075.152 2.108.293 3.121l1.414-1.414A8.964 8.964 0 015 9a8.964 8.964 0 011.121-4.121L5.707 4.293z" />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-gray-300"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 4a1 1 0 00-1 1v1a1 1 0 002 0V5a1 1 0 00-1-1zm0 14a1 1 0 00-1 1v1a1 1 0 002 0v-1a1 1 0 00-1-1zm10-7a1 1 0 00-1-1h-1a1 1 0 000 2h1a1 1 0 001-1zM4 11H3a1 1 0 000 2h1a1 1 0 000-2zm15.071 7.071a1 1 0 00-1.414 0l-.707.707a1 1 0 001.414 1.414l.707-.707a1 1 0 000-1.414zM6.343 5.757a1 1 0 00-1.414 0l-.707.707a1 1 0 001.414 1.414l.707-.707a1 1 0 000-1.414zM17.657 6.464a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM6.343 18.243a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707z" />
-            </svg>
-          )}
-        </button>
+        <label className="swap swap-rotate">
+          {/* Управление состоянием */}
+          <input
+            type="checkbox"
+            onChange={toggleTheme}
+            checked={theme === 'dark'}
+          />
+
+          {/* Иконка солнца */}
+          <svg
+            className="swap-on h-10 w-10 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+          </svg>
+
+          {/* Иконка луны */}
+          <svg
+            className="swap-off h-10 w-10 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+          </svg>
+        </label>
       </header>
       <main className="container mx-auto px-4">
         <ProjectsSection />
